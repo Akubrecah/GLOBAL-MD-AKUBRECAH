e566b196d29953de3a794eab355f656f0b6fba72
Akubrecah <73630463+Akubrecah@users.noreply.github.com>
2024-10-14 21:43:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "923444844060;Qasim";
+const ownervb = process.env.OWNERS || "254739926767; Powel M. M.";
 
 const ownerlist = ownervb.split(';');
 
@@ -22,11 +22,11 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 console.log(global.owner);
 
-global.botNumber = '923051391007'; // Put your number here for pairing directly
+global.botNumber = '254739926767'; // Put your number here for pairing directly
 
-global.mods = ['923444844060'];
-global.prems = ['923444844060']
-global.allowed = ['923444844060']
+global.mods = ['254739926767'];
+global.prems = ['254739926767']
+global.allowed = ['254739926767']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
@@ -62,10 +62,10 @@ global.botname = 'GLOBAL-MD-V3'
 global.premium = 'true'
 global.packname = 'GLOBAL BOT'
 global.author = 'GlobalTechInfo'
-global.igfg = 'https://instagram.com/global.techinc'
-global.fgsc = 'https://github.com/GlobalTechInfo/GLOBAL-MD'
-global.fgyt = 'https://youtube.com/@GlobalTechInfo'
-global.fgpyp = 'https://youtube.com/@GlobalTechInfo'
+global.igfg = 'https://instagram.com/darypowel'
+global.fgsc = 'https://github.com/Globalhttps://github.com/Akubrecah/GLOBAL-MD-AKUBRECAH'
+//global.fgyt = 'https://youtube.com/@GlobalTechInfo'
+//global.fgpyp = 'https://youtube.com/@GlobalTechInfo'
 global.fglog = 'https://i.ibb.co/GVS5Dy9/Ephoto360-com-167054c3328030-2.jpg'
 global.thumb = fs.readFileSync('./assets/qasim.jpg')
 
